6dbae173809e41ad38d076cb42afada92d1a2a95
Joe <48526917+jblsp@users.noreply.github.com>
2024-09-19 20:28:47 -0400

    Add install script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dotfiles
+
+## Set up repo on $HOME
+
+```shell
+sh -c "$(wget https://raw.githubusercontent.com/jblsp/dotfiles/main/install.sh -O -)" && alias config='/usr/bin/git --git-dir=$HOME/.cfggit/ --work-tree=$HOME'
+```
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone --separate-git-dir=$HOME/.cfggit https://github.com/jblsp/dotfiles $HOME/dotfiles.tmp
+cp -r $HOME/dotfiles.tmp/* $HOME/
+rm -rf $HOME/dotfiles.tmp
+git --git-dir=$HOME/.cfggit/ --work-tree=$HOME config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.cfggit/ --work-tree=$HOME update-index --assume-unchanged README.md install.sh
+rm -f $HOME/README.md $HOME/install.sh
+rm -rf dotfiles
