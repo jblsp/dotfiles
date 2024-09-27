87a03db826ef877b394779595e641ed2c931716b
Joe <48526917+jblsp@users.noreply.github.com>
2024-09-27 12:04:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+if [ -d "$HOME/.cfggit" ]; then
+	echo "$HOME/.cfggit" already exists."
+fi
 git clone --separate-git-dir=$HOME/.cfggit https://github.com/jblsp/dotfiles $HOME/dotfiles.tmp
-cp -r $HOME/dotfiles.tmp/* $HOME/
+cp -r $HOME/dotfiles.tmp/. $HOME/
 rm -rf $HOME/dotfiles.tmp
 git --git-dir=$HOME/.cfggit/ --work-tree=$HOME config --local status.showUntrackedFiles no
 git --git-dir=$HOME/.cfggit/ --work-tree=$HOME update-index --assume-unchanged README.md install.sh
 rm -f $HOME/README.md $HOME/install.sh
-rm -rf dotfiles
