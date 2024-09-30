be5f2bd4924fecb5b034e50b9e1257b306b43b51
Joe <48526917+jblsp@users.noreply.github.com>
2024-09-30 15:06:50 -0400

    submodules handled in install

--- install.sh
+++ install.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+
+# Setup git dir on $HOME
 if [ -d "$HOME/.cfggit" ]; then
-	rm -rf $HOME/.cfggit
+	rm -rf "$HOME/.cfggit"
 fi
-git clone --separate-git-dir=$HOME/.cfggit https://github.com/jblsp/dotfiles $HOME/dotfiles.tmp
-cp -r $HOME/dotfiles.tmp/. $HOME/
-rm -rf $HOME/dotfiles.tmp
-git --git-dir=$HOME/.cfggit/ --work-tree=$HOME config --local status.showUntrackedFiles no
-git --git-dir=$HOME/.cfggit/ --work-tree=$HOME update-index --assume-unchanged README.md install.sh
-rm -f $HOME/README.md $HOME/install.sh
+git clone --separate-git-dir="$HOME/.cfggit" https://github.com/jblsp/dotfiles "$HOME/dotfiles.tmp"
+cp -r "$HOME/dotfiles.tmp/." "$HOME/"
+rm -rf "$HOME/dotfiles.tmp"
+
+submodule_paths=$(grep 'path = ' "$HOME/.gitmodules" | sed 's/.*= //')
+for submodule_path in $submodule_paths; do
+	full_path="$HOME/$submodule_path"
+	if [ -d "$full_path" ]; then
+		rm -rf "$full_path"
+	fi
+done
+git --git-dir="$HOME/.cfggit/" --work-tree="$HOME" submodule update --init --recursive
+
+# Update git config
+git --git-dir="$HOME/.cfggit/" --work-tree="$HOME" config --local status.showUntrackedFiles no
+git --git-dir="$HOME/.cfggit/" --work-tree="$HOME" update-index --assume-unchanged "$HOME/README.md" "$HOME/install.sh"
+
+# Cleanup
+rm -f "$HOME/README.md" "$HOME/install.sh"
