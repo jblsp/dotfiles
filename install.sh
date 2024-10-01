107f499c2e47c595f7500b61082f6455a7e23098
Joe <48526917+jblsp@users.noreply.github.com>
2024-10-01 15:18:06 -0400

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Setup git dir on $HOME
 if [ -d "$HOME/.cfggit" ]; then
 	rm -rf "$HOME/.cfggit"
@@ -8,14 +7,6 @@ fi
 git clone --separate-git-dir="$HOME/.cfggit" https://github.com/jblsp/dotfiles "$HOME/dotfiles.tmp"
 cp -r "$HOME/dotfiles.tmp/." "$HOME/"
 rm -rf "$HOME/dotfiles.tmp"
-
-submodule_paths=$(grep 'path = ' "$HOME/.gitmodules" | sed 's/.*= //')
-for submodule_path in $submodule_paths; do
-	full_path="$HOME/$submodule_path"
-	if [ -d "$full_path" ]; then
-		rm -rf "$full_path"
-	fi
-done
 git --git-dir="$HOME/.cfggit/" --work-tree="$HOME" submodule update --init --recursive
 
 # Update git config
