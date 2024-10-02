0da2a4f7e9a026c24780553ce3d8f52496e15ce9
Joe <48526917+jblsp@users.noreply.github.com>
2024-10-02 16:52:05 -0400

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+git_cmd="git --git-dir=\"$HOME/.dotfiles/\" --work-tree=\"$HOME\""
+
 setup() {
 	if [ -d "$HOME/.dotfiles" ]; then
 		rm -rf "$HOME/.dotfiles"
 	fi
 
 	git clone --bare https://github.com/jblsp/dotfiles "$HOME/.dotfiles"
-	git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" checkout -f
+	$git_cmd -f
 
 	submodule_paths=$(grep 'path = ' "$HOME/.gitmodules" | sed 's/.*= //')
 	for submodule_path in $submodule_paths; do
@@ -15,13 +17,17 @@ setup() {
 			rm -rf "$full_path"
 		fi
 	done
-	git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" submodule update --init --recursive
-	git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" config --local status.showUntrackedFiles no
+	$git_cmd submodule update --init --recursive
+	$git_cmd config --local status.showUntrackedFiles no
 }
 
 clean() {
-	git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" update-index --assume-unchanged "$HOME/setup.sh"
+	$git_cmd update-index --assume-unchanged "$HOME/setup.sh"
 	rm -f "$HOME/setup.sh"
+	if [[ "$OSTYPE" != "darwin"* ]]; then
+		$git_cmd update-index --assume-unchanged "$HOME/.macos"
+		rm -f "$HOME/.macos"
+	fi
 }
 
 if [[ "$1" == "--clean-only" ]]; then
