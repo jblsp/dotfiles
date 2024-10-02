ede894708a3f40b12bba76258f5195a7c8a492b6
Joe <48526917+jblsp@users.noreply.github.com>
2024-10-01 22:53:21 -0400

    update install script

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Setup git dir on $HOME
 if [ -d "$HOME/.cfggit" ]; then
 	rm -rf "$HOME/.cfggit"
 fi
+
 git clone --bare https://github.com/jblsp/dotfiles "$HOME/.cfggit"
 git --git-dir="$HOME/.cfggit" --work-tree="$HOME" checkout -f
+
+submodule_paths=$(grep 'path = ' "$HOME/.gitmodules" | sed 's/.*= //')
+for submodule_path in $submodule_paths; do
+	full_path="$HOME/$submodule_path"
+	if [ -d "$full_path" ]; then
+		rm -rf "$full_path"
+	fi
+done
 git --git-dir="$HOME/.cfggit/" --work-tree="$HOME" submodule update --init --recursive
 
-# Update git config
 git --git-dir="$HOME/.cfggit/" --work-tree="$HOME" config --local status.showUntrackedFiles no
 git --git-dir="$HOME/.cfggit/" --work-tree="$HOME" update-index --assume-unchanged "$HOME/README.md" "$HOME/install.sh"
 
-# Cleanup
 rm -f "$HOME/README.md" "$HOME/install.sh"
