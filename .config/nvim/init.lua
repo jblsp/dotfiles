3a628e77cc1aaecba7cccb676b9c74650d7acbc3
Joe <48526917+jblsp@users.noreply.github.com>
2024-10-03 01:02:00 -0400

    Revert "refactor nvim conf"
    This reverts commit 5aa004dbf0d868d3ee0e1ebc4e12f7da6f288294.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,7 @@ vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
-require("options")
-require("keymaps")
-require("autocmds")
-require("lazy")
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+require("config.lazy")
