5aa004dbf0d868d3ee0e1ebc4e12f7da6f288294
Joe <48526917+jblsp@users.noreply.github.com>
2024-10-03 00:36:28 -0400

    refactor nvim conf

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,7 @@ vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
-require("config.lazy")
+require("options")
+require("keymaps")
+require("autocmds")
+require("lazy")
