e39d315aaef23b41a621a5e33e1e350d37e5807c
Joe <48526917+jblsp@users.noreply.github.com>
2024-10-04 17:40:37 -0400

    refactor nvim plugins

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,29 +1,15 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
 vim.opt.mouse = "a"
 vim.opt.showmode = false
 vim.opt.breakindent = true
 vim.opt.undofile = true
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
+vim.opt.ignorecase = true -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.smartcase = true
 vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = "split"
-
-vim.opt.cursorline = false
 vim.opt.scrolloff = 8
 vim.opt.termguicolors = true
 
