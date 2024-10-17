fe1f477eede61f76497079ec3322d47937dcfd67
Joe <48526917+jblsp@users.noreply.github.com>
2024-10-16 20:59:28 -0400

    update nvim plugins

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -32,6 +32,7 @@ return {
 		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
+			css = { "prettier" },
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
