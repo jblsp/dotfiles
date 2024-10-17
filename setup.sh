c05b4a7e1b984165582e14fe4ecbf37474aad563
Joe <48526917+jblsp@users.noreply.github.com>
2024-10-17 19:30:01 -0400

    fix setting upstream in setup

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ setup() {
 
 	git clone --bare https://github.com/jblsp/dotfiles "$repo_dir"
 	$git_cmd checkout -f
-	$git_cmd --set-upstream-to=origin/main
+	$git_cmd push --set-upstream origin main
 
 	submodule_paths=$(grep 'path = ' "$HOME/.gitmodules" | sed 's/.*= //')
 	for submodule_path in $submodule_paths; do
