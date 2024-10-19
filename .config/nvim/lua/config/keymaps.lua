6e34abca100442400a19244b0d795ca2b06c988b
Joe <48526917+jblsp@users.noreply.github.com>
2024-10-18 23:41:09 -0400

    update neovim conf

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,6 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz")
+vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
+vim.keymap.set({ "n", "v" }, "<C-f>", "<C-f>zz")
+vim.keymap.set({ "n", "v" }, "<C-b>", "<C-b>zz")
