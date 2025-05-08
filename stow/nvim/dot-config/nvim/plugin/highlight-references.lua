58efec8725709fdaa8a982b6c3d74ace5b9c3b73
jblsp <48526917+jblsp@users.noreply.github.com>
2025-05-07 21:13:57 -0400

    Add supports_method check in highlight-references

--- stow/nvim/dot-config/nvim/plugin/highlight-references.lua
+++ stow/nvim/dot-config/nvim/plugin/highlight-references.lua
@@ -4,6 +4,11 @@ local TIMEOUT = 250
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("highlight-references-attach", { clear = true }),
   callback = function(event)
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if not client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+      return
+    end
+
     local augroup = vim.api.nvim_create_augroup("highlight-references", { clear = true })
     vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
       buffer = event.buf,
