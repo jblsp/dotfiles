df7d6bebf3100102403ebf70b0f431d93bace5ff
Joe <48526917+jblsp@users.noreply.github.com>
2025-06-22 18:41:21 -0400

    Remove gitfull (was causing issues with ssh)

--- home-manager/config/git.nix
+++ home-manager/config/git.nix
@@ -5,7 +5,6 @@
 }: {
   programs.git = {
     enable = true;
-    package = pkgs.gitFull;
     userName = "Joe";
     userEmail = "48526917+jblsp@users.noreply.github.com";
     ignores = [
@@ -35,17 +34,19 @@
     };
     diff-so-fancy.enable = true;
   };
-  home.file.".config/git/allowedSigners" = {
-    force = true;
-    text = let
-      allowedSigners = {
-        "48526917+jblsp@users.noreply.github.com" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQFryUkLgK9rtC15uPGxqER/K6FkiLLlDZoI0RAWtVg";
-      };
-    in
-      builtins.concatStringsSep "\n" (
-        builtins.map
-        (key: "${key} ${toString allowedSigners.${key}}")
-        (builtins.attrNames allowedSigners)
-      );
+  home = {
+    file.".config/git/allowedSigners" = {
+      force = true;
+      text = let
+        allowedSigners = {
+          "48526917+jblsp@users.noreply.github.com" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQFryUkLgK9rtC15uPGxqER/K6FkiLLlDZoI0RAWtVg";
+        };
+      in
+        builtins.concatStringsSep "\n" (
+          builtins.map
+          (key: "${key} ${toString allowedSigners.${key}}")
+          (builtins.attrNames allowedSigners)
+        );
+    };
   };
 }
