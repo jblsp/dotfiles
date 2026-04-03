e2e9b605dd5e8df6d88f20c4920936c226154a31
Joe <joe@jblsp.com>
2026-04-03 15:17:29 -0400

    nvim: update oc confg

--- home/dot_config/nvim/lua/joe/plugins/opencode.lua
+++ home/dot_config/nvim/lua/joe/plugins/opencode.lua
@@ -2,6 +2,8 @@ return {
   "nickjvandyke/opencode.nvim",
   version = "*",
   config = function()
+    local opencode = require("opencode")
+
     ---@type opencode.Opts
     vim.g.opencode_opts = {
       lsp = {
@@ -9,22 +11,21 @@ return {
       },
     }
 
-    -- Recommended/example keymaps
     vim.keymap.set("n", "<leader>op", function()
-      require("opencode").ask("", { submit = true })
+      opencode.ask("", { submit = true })
     end, { desc = "Ask opencode…" })
     vim.keymap.set("n", "<leader>oa", function()
-      require("opencode").select()
+      opencode.select()
     end, { desc = "Execute opencode action…" })
     vim.keymap.set({ "n" }, "<leader>ot", function()
-      require("opencode").toggle()
+      opencode.toggle()
     end, { desc = "Toggle opencode" })
 
     vim.keymap.set({ "n", "x" }, "<leader>or", function()
-      return require("opencode").operator("@this ")
+      return opencode.operator("@this ")
     end, { desc = "Add range to opencode", expr = true })
     vim.keymap.set("n", "<leader>ol", function()
-      return require("opencode").operator("@this ") .. "_"
+      return opencode.operator("@this ") .. "_"
     end, { desc = "Add line to opencode", expr = true })
   end,
 }
