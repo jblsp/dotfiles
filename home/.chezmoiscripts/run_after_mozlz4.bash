ce55b7d00a45361c6af3c6b37bd94ec4a5b2bf4d
Joe <joe@jblsp.com>
2026-04-25 17:36:06 -0400

    New package installation

--- home/.chezmoiscripts/run_after_mozlz4.bash
+++ home/.chezmoiscripts/run_after_mozlz4.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Utility to handle files read by Mozilla programs that need to be in mozlz4 format
+# Requires https://github.com/cions/mozlz4/
 
 for profile_dir in $HOME/.config/mozilla/firefox/profiles/*/; do
   if [[ -f "${profile_dir}search.json" ]]; then
